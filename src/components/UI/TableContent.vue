99f3174a8ed70ec958847f6310d9dcf41318034a
preye omusuku <omusukup@yahoo.com>
2023-05-30 19:51:12 +0100

    separate table component

--- src/components/UI/TableContent.vue
+++ src/components/UI/TableContent.vue
@@ -4,80 +4,25 @@
     :rows="items"
     :columns="columns"
     row-key="id"
-    :rows-per-page-options="[10, 25, 50]"
-    :pagination="true"
+    :rows-per-page-options="[5, 25, 50]"
+    pagination="true"
     :filter="filter"
-    :filter-method="filterMethod"
   >
-    <template v-slot:top>
-      <div class="content">
-        <h6>Businesses: 34</h6>
-        <q-input
-          v-model="filter"
-          debounce="300"
-          outlined
-          placeholder="Search using business name or business id"
-          class="q-pa-md search_btn"
-        />
-      </div>
-    </template>
   </q-table>
 </template>
 
 <script>
-import items from "../../items";
+import { items, columns } from "../../items";
 
 export default {
   data() {
     return {
-      items: items,
-      columns: [
-        {
-          name: "Business Name",
-          required: true,
-          label: "Business Name",
-          align: "left",
-          field: "name",
-          sortable: true,
-        },
-        {
-          name: "Business ID",
-          required: true,
-          label: "Business ID",
-          align: "left",
-          field: "description",
-          sortable: true,
-        },
-        {
-          name: "Verification Status",
-          required: true,
-          label: "Verification Status",
-          align: "left",
-          field: "name",
-          sortable: true,
-        },
-        {
-          name: "Business Type",
-          required: true,
-          label: "Business Type",
-          align: "left",
-          field: "name",
-          sortable: true,
-        },
-        {
-          name: "Date Created",
-          required: true,
-          label: "Date Created",
-          align: "left",
-          field: "name",
-          sortable: true,
-        },
-
-        // Add more columns as needed
-      ],
+      items,
+      columns,
       filter: "",
     };
   },
+
   // computed: {
   //   filterMethod(row, search) {
   //     return (
@@ -94,6 +39,9 @@ export default {
 .content_table {
   margin-left: 2.5%;
   margin-right: 2.5%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 .search_btn {
   width: 23rem;
@@ -106,5 +54,17 @@ export default {
   justify-content: space-between;
   border: 1px solid red;
   width: 100%;
+}
+
+.status-approved {
+  color: green;
+}
+
+.status-pending {
+  color: orange;
+}
+
+.status-queried {
+  color: red;
 }
 </style>
