06ff8c65c8525ef31b560607c7abec982328c480
kingjamesx <talk2james.uj@gmail.com>
2023-05-30 08:11:52 +0100

    add business page

---
+++ src/components/BusinessUI/BaseBusiness.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+        <q-tabs
+          v-model="this.tab"
+          dense
+          color="#222222"
+          active-color="#121212"
+          indicator-color="#121212"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="All businesses" label="All businesses" />
+          <q-tab name="Approved businesses" label="Approved businesses" class="tab" />
+          <q-tab name="Pending Approval" label="Pending Approval" class="tab" />
+          <q-tab name="Queried Approvals" label="Queried Approvals" class="tab" />
+
+        </q-tabs>
+        <q-separator inset/>
+         <BusinessTableHeader/>
+        <q-tab-panels v-model="this.tab" animated>
+          <q-tab-panel name="All businesses">
+            <BaseTable/>
+          </q-tab-panel>
+
+          <q-tab-panel name="Approved businesses">
+            <BaseTable/>
+          </q-tab-panel>
+
+          <q-tab-panel name="Pending Approval">
+            <BaseTable/>
+          </q-tab-panel>
+          <q-tab-panel name="Queried Approvals">
+            <BaseTable/>
+          </q-tab-panel>
+        </q-tab-panels> 
+    </div>
+</template>
+
+<script>
+ import { ref } from 'vue'
+ import BaseTable from 'src/components/UI/BaseTable.vue';
+ import BusinessTableHeader from 'src/components/BusinessUI/BusinessTableHeader.vue'
+//  const tab=ref('mails')
+
+ export default({
+  components:{BaseTable,BusinessTableHeader},
+   data(){
+    return{
+      tab:"All businesses"
+    }
+      
+   }
+ })
+</script>
+
+<style lang="scss" scoped>
+.container{
+    padding: 2rem;
+}
+.tab{
+ 
+}
+</style>
--- src/pages/BusinessPage.vue
+++ src/pages/BusinessPage.vue
@@ -1,47 +1,12 @@
 <template>
-    <div class="container">
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="All businesses" label="All businesses" />
-          <q-tab name="Approved businesses" label="Approved businesses" />
-          <q-tab name="Pending Approval" label="Pending Approval" />
-          <q-tab name="Queried Approvals" label="Queried Approvals" />
-
-        </q-tabs>
-        <q-separator />
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="All businesses">
-            <div class="text-h6">Mails</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-
-          <q-tab-panel name="alarms">
-            <div class="text-h6">Alarms</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-
-          <q-tab-panel name="movies">
-            <div class="text-h6">Movies</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-        </q-tab-panels> 
+    <div>
+    <BaseBusiness/>
     </div>
 </template>
 
-<script setup>
- import { ref } from 'vue'
- const tab=ref('mails')
+<script >
+import BaseBusiness from 'src/components/BusinessUI/BaseBusiness.vue';
+ export default{
+     components:{BaseBusiness}
+ }
 </script>
-
-<style lang="scss" scoped>
-.container{
-    padding: 1rem;
-}
-</style>
