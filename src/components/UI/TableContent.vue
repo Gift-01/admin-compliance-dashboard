6ca9bd1b5b59a87b8f5ed2d97e5bf6260d62cec7
preye omusuku <omusukup@yahoo.com>
2023-05-30 04:36:53 +0100

    Update Dashboard

---
+++ src/components/UI/TableContent.vue
@@ -0,0 +1,110 @@
+<template>
+  <q-table
+    class="content_table"
+    :rows="items"
+    :columns="columns"
+    row-key="id"
+    :rows-per-page-options="[10, 25, 50]"
+    :pagination="true"
+    :filter="filter"
+    :filter-method="filterMethod"
+  >
+    <template v-slot:top>
+      <div class="content">
+        <h6>Businesses: 34</h6>
+        <q-input
+          v-model="filter"
+          debounce="300"
+          outlined
+          placeholder="Search using business name or business id"
+          class="q-pa-md search_btn"
+        />
+      </div>
+    </template>
+  </q-table>
+</template>
+
+<script>
+import items from "../../items";
+
+export default {
+  data() {
+    return {
+      items: items,
+      columns: [
+        {
+          name: "Business Name",
+          required: true,
+          label: "Business Name",
+          align: "left",
+          field: "name",
+          sortable: true,
+        },
+        {
+          name: "Business ID",
+          required: true,
+          label: "Business ID",
+          align: "left",
+          field: "description",
+          sortable: true,
+        },
+        {
+          name: "Verification Status",
+          required: true,
+          label: "Verification Status",
+          align: "left",
+          field: "name",
+          sortable: true,
+        },
+        {
+          name: "Business Type",
+          required: true,
+          label: "Business Type",
+          align: "left",
+          field: "name",
+          sortable: true,
+        },
+        {
+          name: "Date Created",
+          required: true,
+          label: "Date Created",
+          align: "left",
+          field: "name",
+          sortable: true,
+        },
+
+        // Add more columns as needed
+      ],
+      filter: "",
+    };
+  },
+  // computed: {
+  //   filterMethod(row, search) {
+  //     return (
+  //       row.name.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
+  //       row.description.toLowerCase().indexOf(search.toLowerCase()) !== -1
+  //       // Add more properties to search on as needed
+  //     );
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+.content_table {
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+}
+.search_btn {
+  width: 23rem;
+  text-align: center;
+  margin-top: 1rem;
+}
+.content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  border: 1px solid red;
+  width: 100%;
+}
+</style>
---
+++ src/items.js
@@ -0,0 +1,143 @@
+export default [
+  {
+    id: 1,
+    name: "Item 1",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 2,
+    name: "Item 2",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 3,
+    name: "Item 3",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 4,
+    name: "Item 4",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 5,
+    name: "Item 5",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 6,
+    name: "Item 6",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 7,
+    name: "Item 7",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 8,
+    name: "Item 8",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 9,
+    name: "Item 9",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 10,
+    name: "Item 10",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 11,
+    name: "Item 11",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 12,
+    name: "Item 12",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  {
+    id: 13,
+    name: "Item 13",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 14,
+    name: "Item 14",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 15,
+    name: "Item 15",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  {
+    id: 16,
+    name: "Item 16",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 17,
+    name: "Item 11",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 18,
+    name: "Item 18",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  {
+    id: 19,
+    name: "Item 19",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  {
+    id: 20,
+    name: "Item 20",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+  {
+    id: 21,
+    name: "Item 21",
+    description: "Description of Item 1",
+    // Add more properties as needed
+  },
+  {
+    id: 22,
+    name: "Item 22",
+    description: "Description of Item 2",
+    // Add more properties as needed
+  },
+  // Add more items as needed
+];
