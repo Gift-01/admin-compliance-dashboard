a6ca0d1d782bc9153091903f4ee7bb94dd51decd
kingjamesx <talk2james.uj@gmail.com>
2023-05-30 17:15:38 +0100

    add nested route

---
+++ src/pages/BusinessDetailsPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <h1>Business details Page</h1>
+    </div>
+</template>
+
+<script>
+
+export default({
+    mounted() {
+      this.getid()
+},
+     methods:{
+        getid(){
+        console.log(this.$route)
+        }
+     }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
